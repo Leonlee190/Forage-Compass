6b1d796fbfc007e93f6268a9d734b56f21c798f7
Camilo <camilo3@pdx.edu>
2021-05-17 01:23:58 -0700

    commented, I still don't quite get location.js though, api key not hidden yet

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,29 +1,61 @@
+// https://www.digitalocean.com/community/tutorials/how-to-integrate-the-google-maps-api-into-react-applications
 import React, { Component } from "react";
-import { Map, GoogleApiWrapper } from "google-maps-react";
-import "./map.css";
-
-const mapStyles = {
-  width: "100%",
-  height: "100%",
-};
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
+import "./map.css"; // minor styling here
+import CurrentLocation from "./location"; // centers map @ curr loc
 
 export class MapContainer extends Component {
+  state = {
+    // this is all stuff from google-react tutorial
+    showingInfoWindow: false, // hides/shows infoWindow
+    activeMarker: {}, // shows marker on click
+    selectedPlace: {}, // shows info window to selected marker
+  };
+
+  // sets active marker as this one, props as the location of the marker
+  // I think? .. and switches the state of the infowindow on/off
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: !this.state.showingInfoWindow,
+    });
+
+  // hides the info window
+  onClose = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null,
+      });
+    }
+  };
+
   render() {
     return (
-      <Map
-        className="my_map"
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-          lat: 45.5191,
-          lng: -122.675,
-        }}
-      />
+      // current location tries to get your location and then centers the map around it.  storred in map/location.js
+      <CurrentLocation centerAroundCurrentLocation google={this.props.google}>
+        {/*This I think just puts a single marker in the center of screen, for testing/from tutorial*/}
+        <Marker onClick={this.onMarkerClick} name={"Current Location"} />
+        {/* this is where we put the info window */}
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClick={this.onClose}
+        >
+          {/* this is where the info for infowindow is displayed...
+            can later put in, ratings, size, picture?  whatever */}
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </CurrentLocation>
     );
   }
 }
 
+// not sure how to hide the apiKey quite yet...
+// or if I did it right... differentiated from documentation
 MapContainer = GoogleApiWrapper({
   apiKey: "AIzaSyBDg6EoMTVDIdl2PUR_ZaJ5br6fuMjGWRQ",
 })(MapContainer);
