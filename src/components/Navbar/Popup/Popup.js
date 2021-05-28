33219846ecca13c8af18017d64bd12d2e3baa7b9
Lydeon <leonlee190@gmail.com>
2021-05-28 00:18:07 -0700

    added popup add form

---
+++ src/components/Navbar/Popup/Popup.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Popup.css";
+import { Link } from "react-router-dom";
+import * as aiIcons from "react-icons/ai";
+
+const Popup = (props) => {
+  return (
+    <div className="modal">
+      <div className="modal_content">
+        <Link to="#" className="modal_close" onClick={props.handleClose}>
+          <aiIcons.AiOutlineClose />
+        </Link>
+        <form className="modal-form">
+          <label htmlFor="name">Location Name:</label>
+          <input type="text" id="name" name="name" size="50"></input>
+          <label htmlFor="type">Type:</label>
+          <select id="type" name="type" form="type">
+            <option value="blackberry">Blackberry</option>
+            <option value="blueberry">Blueberry</option>
+            <option value="huckleberry">Huckleberry</option>
+            <option value="raspberry">Raspberry</option>
+            <option value="marionberry">Marionberry</option>
+            <option value="chantrelle">Chantrelle Mushroom</option>
+            <option value="morel">Morel Mushroom</option>
+            <option value="matsutake">White Matsutake Mushroom</option>
+            <option value="oyster">Oyster Mushroom</option>
+            <option value="porcini">Porcini Mushroom</option>
+            <option value="apple">Apple</option>
+            <option value="lemon">Lemon</option>
+            <option value="peach">Peach</option>
+            <option value="persimmon">Persimmon</option>
+            <option value="pear">Pear</option>
+          </select>
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
