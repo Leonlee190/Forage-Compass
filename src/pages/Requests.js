6acef3db2311a24b233ef56a19f6909086dc78aa
Camilo <camilo3@pdx.edu>
2021-06-04 22:00:50 -0700

    got styling going.  tried to change submission behavior, might be barking up wrong tree

--- src/pages/Requests.js
+++ src/pages/Requests.js
@@ -1,6 +1,15 @@
+// import { Redirect } from "react-router";
 import "./Contacts.css";
+// import { Link } from "react-router-dom";
 
 function Requests() {
+  const handleSubmit = (event) => {
+    // event.preventDefault(); // we want default.
+    // default is to actually post, but then returns page just with text: Support item created
+    alert("Thank you! We'll look into it.");
+    // can't figure out how to a. redirect, or b. respond to different post responses.
+  };
+
   return (
     <div className="wholePage">
       <h1>Request</h1>
@@ -8,16 +17,23 @@ function Requests() {
         If you want a specific item, type, or functionality to be added please
         let us know!
       </h3>
-      <form className="report-form" action="/support" method="post">
+      <form
+        className="report-form"
+        action="/support"
+        method="post"
+        onSubmit={handleSubmit}
+      >
         <label htmlFor="category">Type of Request:</label>
         <select name="category" id="category">
           <option value="Feature">Feature Request</option>
           <option value="Bug">Report a Bug</option>
           <option value="Data">Data Changes</option>
         </select>
-        <label htmlFor="request">Request: </label>
-        <input type="text" id="report" name="message"></input>
+        <label htmlFor="report">Request: </label>
+        <textarea id="report" name="message"></textarea>
+        {/* <Link to={"/"}> */}
         <input type="submit" id="report-submit"></input>
+        {/* </Link> */}
       </form>
     </div>
   );
